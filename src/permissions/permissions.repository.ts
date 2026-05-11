4c8b329bd003e025d63807a5d0e61091201deecd
Vasileios Kefalas <hello@vasileioskefalas.com>
2026-05-11 11:47:23 +0300

    Read queries sequentially, fixed the error

--- src/permissions/permissions.repository.ts
+++ src/permissions/permissions.repository.ts
@@ -12,32 +12,33 @@ export async function fetchPermissions(userId: string): Promise<UserPermissions>
   const client: PoolClient = await pool.connect();
 
   try {
-    const [toCreate, onBankAccount, onCustomer, onUser] = await Promise.all([
-      client.query(
-        `SELECT subsystem_id, permission
-         FROM public.permissions_to_create
-         WHERE user_account_id = $1`,
-        [userId]
-      ),
-      client.query(
-        `SELECT bank_account_id, subsystem_id, permission, customer_id
-         FROM public.permissions_on_bank_account
-         WHERE user_account_id = $1`,
-        [userId]
-      ),
-      client.query(
-        `SELECT subsystem_id, permission, customer_id
-         FROM public.permissions_on_customer
-         WHERE user_account_id = $1`,
-        [userId]
-      ),
-      client.query(
-        `SELECT subsystem_id, permission
-         FROM public.permissions_on_user
-         WHERE user_account_id = $1`,
-        [userId]
-      ),
-    ]);
+    const toCreate = await client.query(
+      `SELECT subsystem_id, permission
+       FROM public.permissions_to_create
+       WHERE user_account_id = $1`,
+      [userId]
+    );
+
+    const onBankAccount = await client.query(
+      `SELECT bank_account_id, subsystem_id, permission, customer_id
+       FROM public.permissions_on_bank_account
+       WHERE user_account_id = $1`,
+      [userId]
+    );
+
+    const onCustomer = await client.query(
+      `SELECT subsystem_id, permission, customer_id
+       FROM public.permissions_on_customer
+       WHERE user_account_id = $1`,
+      [userId]
+    );
+
+    const onUser = await client.query(
+      `SELECT subsystem_id, permission
+       FROM public.permissions_on_user
+       WHERE user_account_id = $1`,
+      [userId]
+    );
 
     return {
       permissions_to_create: toCreate.rows,
